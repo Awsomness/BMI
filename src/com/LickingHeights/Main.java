cb073aab75c5fc5a10e47efafe73fd6432bcb090
Nathanial <nathanial.kilpatrick.2022@student.lhschools.org>
2019-09-23 09:37:33 -0400

    Added"Scanner keyboard;"to main method

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,38 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	// write your code here
+        Scanner keyboard;
+        int inches,pounds;
+        double conversionFrominchesTometers = 0.0254 ,conversionFrompoundsTokilograms = 0.453592 ,meters,kg,bmi;
+
+        //Initial
+        System.out.println("What is your height in inches?");
+
+        keyboard = new Scanner(System.in);
+
+        inches = keyboard.nextInt();
+
+        meters=inches*0.0254;
+
+        System.out.println(meters+"meters");
+
+        System.out.println("What is your weight in pounds?");
+        pounds=keyboard.nextInt();
+        kg=pounds*0.453592;
+        System.out.println(kg+ "kg");
+        bmi=kg/(meters*meters);
+        System.out.println("Your bmi is...");
+        System.out.println(bmi);
+
+
+
+
+
+    }
+}
